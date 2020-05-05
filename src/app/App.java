aaaedcfa5bda55689a042718fd0983a27a3e0a4a
JC <jcal.d.as.1.9.99+vivasgithub@gmail.com>
2020-05-05 13:44:09 +0200

    Añadir lectura de archivo mediante stdin o argumento.

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,18 @@
 package app;
 
+import java.io.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        InputStream inputStream = null;
+        try {
+            if (args.length > 0) {
+                inputStream = new FileInputStream(args[0]);
+            } else {
+                inputStream = System.in;
+            }
+        } finally {
+            inputStream.close();
+        }
     }
 }
